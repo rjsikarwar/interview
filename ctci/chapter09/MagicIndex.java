7f0b92f57dd4bee7bc096c4318ba807e900d2762
ZhaohuiZh <bravezhaohui@gmail.com>
2013-08-26 10:01:43 -0400

    follow up method and recursion

--- ctci/chapter09/MagicIndex.java
+++ ctci/chapter09/MagicIndex.java
@@ -26,6 +26,32 @@ public class MagicIndex{
     return -1;
   }
 
+  public static int findMagicIndex_RecursionFollowUp(int[] array){
+    int len = array.length;
+    findMagicIndex_RecursionFollowUp(array, 0, len - 1);
+  }
+
+  public static int findMagicIndex_RecursionFollowUp(int[] array, int minIndex, int maxIndex){
+    if(minIndex > maxIndex || minIndex < 0 || maxIndex >= array.length){
+      return -1;
+    }
+    int middleIndex = (minIndex + maxIndex) >>> 1;
+    if(array[middleIndex] == middleIndex){
+      return middleIndex;
+    }
+    int result = -1;
+    
+    int tmpIndex = Math.max(middleIndex + 1, array[middleIndex]);
+    result = findMagicIndex_RecursionFollowUp(array, tmpIndex, maxIndex);
+    if(result >= 0){
+      return result;
+    }
+
+    int tmpIndex = Math.min(middleIndex - 1, array[middleIndex]);
+    return findMagicIndex_RecursionFollowUp(array, minIndex, tmpIndex);
+    
+  }
+
   public static void main(String[] args){
     int[] array = new int[]{1,2,3,5,8,9};
     System.out.print(findMagicIndex(array));
