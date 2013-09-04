08edc41628599c664f66a20969f14b5b234cc5ff
ZhaohuiZh <bravezhaohui@gmail.com>
2013-09-04 00:17:29 -0400

    add edit distance

---
+++ others/EditDistance.java
@@ -0,0 +1,12 @@
+package others;
+
+/**
+* write an algorithm to calculate the edit distance between two strings str1 and str2,
+* which is the number of insertation, deletion and modification to go from str1 to str2
+* Also known as "Levenshtein Distance"
+*/
+public class EditDistance{
+  public int getEditDistance(String str1, String str2){
+
+  }
+}
