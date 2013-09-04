872810bad07272200aae7849a376368f4bc00b73
ZhaohuiZh <bravezhaohui@gmail.com>
2013-09-04 17:54:19 -0400

    add more space effecient method

--- others/EditDistance.java
+++ others/EditDistance.java
@@ -6,6 +6,8 @@ package others;
 * Also known as "Levenshtein Distance"
 */
 public class EditDistance{
+
+  // implemented using a m X n matrix
   public int getEditDistance(String str1, String str2){
     int len1 = str1.length();
     int len2 = str2.length();
@@ -27,13 +29,53 @@ public class EditDistance{
     for(int i = 1; i < len1 + 1; i++){
       for(int j = 1; j < len2 + 1; j++){
         if(str1.charAt(i - 1) == str2.charAt(j - 1)){
-          map[i][j] = min(map[i - 1][j] + 1, map[i][j - 1] + 1, map[i - 1][j - 1]);
+          map[i][j] =  map[i - 1][j - 1];
         }else{
           map[i][j] = min(map[i - 1][j] + 1, map[i][j - 1] + 1, map[i - 1][j - 1] + 1);
         }
       }
     }
     return map[len1][len2];
+  }
+
+  
+  //implement using two rows
+
+  public int getEditDistance_twoRows(String str1, String str2){
+    if(str1.equals(str2)){
+      return 0;
+    }
+    int len1 = str1.length();
+    int len2 = str2.length();
+    if(len1 == 0){
+      return len2;
+    }
+    if(len2 == 0){
+      return len1;
+    }
+
+    int[] array1 = new int[len2 + 1];
+    int[] array2 = new int[len2 + 1];
+
+    for(int i = 0; i < len2 + 1; i++){
+      array1[i] = i;
+    }
+
+    for(int i = 1; i < len1 + 1; i++){
+      array2[0] = i;
+      for(int j = 1; j < len2 + 1; j++){
+        if(str1.charAt(i - 1) == str2.charAt(j - 1)){
+          array2[j] = array1[j - 1];
+        }else{
+          array2[j] = min(array2[j - 1] + 1, array1[j] + 1, array1[j - 1] + 1);
+        }
+      }
+      for(int j = 0; j < len2 + 1; j++){
+        array1[j] = array2[j];
+      }
+
+    }
+    return array2[len2];
   }
 
   private int min(int x, int y, int z){
@@ -44,6 +86,6 @@ public class EditDistance{
 
   public static void main(String[] args){
     EditDistance ed = new EditDistance();
-    System.out.println(ed.getEditDistance("Saturday", "Sunday"));
+    System.out.println(ed.getEditDistance_twoRows("Saturday", "Sunday"));
   }
 }
