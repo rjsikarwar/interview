ecd87afd4134f11e28a44e2e1df561ce5b527acd
ZhaohuiZh <bravezhaohui@gmail.com>
2013-10-08 18:04:04 -0400

    google counter

---
+++ interview/google/Counter.java
@@ -0,0 +1,10 @@
+/**
+* Implement this counter interface
+*/
+interface Counter {
+    void increment();
+    int lastSecCount();
+    int lastMinCount();
+    int lastHourCount();
+    int lastDayCount();
+}
