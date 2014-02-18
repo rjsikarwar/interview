848417ac8942f36cb97aa6c45a113ad8ad4a230b
Zhaohui <bravezhaohui@gmail.com>
2014-02-18 00:34:54 -0500

    merge sort

--- template/sort/MergeSort.java
+++ template/sort/MergeSort.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 
 public class MergeSort{
   private static int[] array;
@@ -14,17 +15,33 @@ public class MergeSort{
     mergeSort(middle + 1, end);
     merge(begin, middle, end);
   }
-  public static void merge(int begin, int middle, ind end){
+  public static void merge(int begin, int middle, int end){
     int [] temp = new int[array.length];
     int i = begin;
     int j = middle + 1;
-    while(i <= middle && j <== end){
+    int k = begin;
+    while(i <= middle && j <= end){
       if(array[i] > array[j]){
-        swap(i, j);
+        temp[k++] = array[j++];
+      }else{
+        temp[k++] = array[i++];
       }
+    }
+    while(i<=middle){
+      temp[k++] = array[i++];
+    }
+    while(j <= end){
+      temp[k++] = array[j++];
+    }
+    System.out.println(Arrays.toString(temp));
+    for(int m = begin; m <=end; m++){
+      array[m] = temp[m];
     }
   }
   public static void main(String[] args){
-    
+    array = new int[]{2, 3, 1, 7, 3, 0, -5, 9, 4};
+    System.out.println(Arrays.toString(array));
+    mergeSort();
+    System.out.println(Arrays.toString(array));
   }
 }
