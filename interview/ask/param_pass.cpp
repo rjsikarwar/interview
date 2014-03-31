c658178ed560243042dc755af2a0854dca617ebd
Zhaohui <bravezhaohui@gmail.com>
2014-03-31 16:25:07 -0400

    a c++ question

---
+++ interview/ask/param_pass.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+class Wrapper{
+  public:
+    Wrapper(int i = 0){
+      dig = i;
+    }
+    int & getDig(){
+      return dig;
+    }
+  private:
+    int dig;
+};
+
+void mul( int a ){
+  a = 2 * a;
+}
+
+int main(){
+  Wrapper w(1);
+  mul(w.getDig());
+  printf("%d\n", w.getDig());
+  return 0;
+}
