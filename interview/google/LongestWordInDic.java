ce8b4f04f795c8cf0770646537a3d19c01cb78e6
Zhaohui <bravezhaohui@gmail.com>
2014-04-08 21:01:14 -0400

    wa keng

---
+++ interview/google/LongestWordInDic.java
@@ -0,0 +1,9 @@
+/**
+ * Suppose that you have a dictionary of words and a collection of letters
+ * Find all of the longest words in the dictionary that you can form using letters in your collection:
+ */
+
+public class LongestWordInDic{
+
+
+}
